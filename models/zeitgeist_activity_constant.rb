710aabbddc14cf151731111d0de175ccccf68d80
amrutjadhav <amrutjadhav2294@gmail.com>
2017-06-30 08:09:56 +0530

    From neo4jrbv8.0 we can't set index from model itself. For that seperated migration has to write.

--- models/zeitgeist_activity_constant.rb
+++ models/zeitgeist_activity_constant.rb
@@ -2,6 +2,6 @@ class  ZeitgeistActivityConstant
   include Neo4j::ActiveNode
 
   id_property :neo_id
-  property :constant_id, type: Integer, index: :exact
+  property :constant_id, type: Integer
   property :name, type: String
 end
